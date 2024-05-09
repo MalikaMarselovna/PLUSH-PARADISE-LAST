3d5f6a333ec5df096473b5de798c6082d17d6a60
MalikaMarselovna <148754005+MalikaMarselovna@users.noreply.github.com>
2024-05-10 01:53:07 +0600

    add folder which called Apislades

---
+++ src/apiFolder/SliderApi.js
@@ -0,0 +1,20 @@
+export const ApiSlides = [
+    {
+        src: "https://amigurumi.today/wp-content/uploads/2023/06/Free-pink-giraffe-crochet-pattern.jpg",
+        content: {
+            h2: "Hey this is awesome today deal",
+            p: "Up to 40% off",
+        },
+        background: 'bg-[#fff]'
+    },
+    {
+        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg",
+        active: true,
+        content: {
+            h2: "Avail Awesome Deals this week!",
+            p: "Up to 40% off"
+        },
+        background: 'bg-[#fff]'
+    },
+    
+];
