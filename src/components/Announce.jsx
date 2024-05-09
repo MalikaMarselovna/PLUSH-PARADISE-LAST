ad3c10775aea894762f64e83539e277e258f78d9
MalikaMarselovna <148754005+MalikaMarselovna@users.noreply.github.com>
2024-05-09 21:25:37 +0600

    start my diplom project

---
+++ src/components/Announce.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Close} from '@material-ui/icons';
+
+function Announce() {
+  return (
+    <div className='bg-[#f92e9e] font-bold text-white flex'>
+        <h2>Announce 40%</h2>
+        <Close/>
+    </div>
+  )
+}
+
+export default Announce
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Announce from '../components/Announce';
+
+
+function Home() {
+  return (
+    <div>
+        <Announce/>
+    </div>
+  )
+}
+ 
+export default Home;
