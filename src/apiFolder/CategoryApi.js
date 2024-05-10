6b1275df07cb176d8969f3ad2d017b0dea00696b
MalikaMarselovna <148754005+MalikaMarselovna@users.noreply.github.com>
2024-05-10 22:13:10 +0600

    add title and src for catigories cards

--- src/apiFolder/CategoryApi.js
+++ src/apiFolder/CategoryApi.js
@@ -1,37 +1,37 @@
 export const ApiCotigories = [
     {
-        title: '',
-        src: "https://amigurumi.today/wp-content/uploads/2023/06/Free-pink-giraffe-crochet-pattern.jpg"
-    },
-    {
         title: 'ANIMALS',
-        src: "https://i.pinimg.com/564x/d2/f3/ae/d2f3ae6b61fa372d0752cac645ab5c4f.jpg"
+        src: "https://i.pinimg.com/564x/d2/f8/81/d2f8816df7b1eefa8bf204ce4b7f730a.jpg"
       
     },
     {
         title: 'DOLLS',
-        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg"
+        src: "https://i.pinimg.com/564x/0c/83/df/0c83df989a240a07f2699a8b9bc84c39.jpg"
     },
     {
-        title: 'RATTLES',
-        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg"
+        title: 'FOR BABIES',
+        src: "https://i.pinimg.com/564x/13/f0/14/13f014d13449a81c7426e6d6c38af609.jpg"
     },
     {
         title: 'CARTOON CHARACTERS',
-        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg"
+        src: "https://i.pinimg.com/564x/c0/d7/7b/c0d77bcf370d188c6d354d014f841276.jpg"
     },
     {
         title: 'PAIRS',
-        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg"
+        src: "https://i.pinimg.com/564x/9c/c2/40/9cc2403f0470866a875b0f0ee231fe11.jpg"
+    },
+    {
+        title: 'MEAL',
+        src: "https://i.pinimg.com/564x/f1/d2/14/f1d214a82d42b8e4a2d83d5a5d1e5c16.jpg"
     },
     {
         title: 'GEOMETRIC FIGURES',
-        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg"
+        src: "https://i.pinimg.com/564x/99/c3/b9/99c3b9875ffd7309bb49604e560214f2.jpg"
     },
     {
         title: 'KEY RINGS',
-        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg"
-    },
+        src: "https://i.pinimg.com/564x/7b/9b/b9/7b9bb9dd2fdfc47e2609840af65029a4.jpg"
+    }, 
     
     
 ];
