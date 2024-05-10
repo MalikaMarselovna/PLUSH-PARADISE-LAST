d6df78fde47e429a79ee73c7be77a7cdc8ee39b7
MalikaMarselovna <148754005+MalikaMarselovna@users.noreply.github.com>
2024-05-10 20:26:34 +0600

    finished making the main page slider

--- src/apiFolder/SliderApi.js
+++ src/apiFolder/SliderApi.js
@@ -8,6 +8,16 @@ export const ApiSlides = [
         background: 'bg-[#fff]'
     },
     {
+        src: "https://i.pinimg.com/564x/d2/f3/ae/d2f3ae6b61fa372d0752cac645ab5c4f.jpg",
+        active: true,
+        content: {
+            h2: "Avail Awesome Deals this week!",
+            p: "Up to 40% off"
+        },
+        background: 'bg-[#fff]'
+    },
+    
+    {
         src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg",
         active: true,
         content: {
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,7 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
 import React, { useState } from 'react'
-import { ApiSlides } from '../apiFolder/SliderApi';
+import { ApiSlides } from '../apiFolder/SliderApi'
+import './Slider.css'
 
 
 function Slider() {
@@ -10,17 +11,60 @@ function Slider() {
 
     //style
     const arrowStyle = " rounded-full bg-grey flex  shadow-sm items-center  justify-center hover:cursor-pointer";
+
+    const nextSlide =()=> {
+        if(activeSlide ===slides.length - 1)
+        {
+            setActiveSlide(0)
+        }
+        else{
+            setActiveSlide(activeSlide + 1)
+        }
+    }
+
+    const prvslide =()=> {
+        if(activeSlide === 0){
+            setActiveSlide(slides.length - 1)
+        }
+        else{
+            setActiveSlide(activeSlide - 1)
+        }
+
+    }
+
+
   return (
     <div className="parentDiv h-540px bg-white flex items-center justify-between">
         {/* leftarrow */}
         <div className={arrowStyle}>
-            <ArrowLeftOutlined  style={{fontSize : '50px'}}/>
+            <ArrowLeftOutlined  style={{fontSize : '50px'}} onClick={prvslide}/>
         </div>
         {/* slide div */}
+        {slides.map((slide, index)=>{
+            if(index ===activeSlide){
+                return(
+                    <div className={`wrapper flex w-[100%] h-[600px] justify-center items-center shadow-2xl rounded-lg border-[#c0c0c0] border-10px   mt-[10px] overflow-hidden relative ` + slide.background}>
+                    <div className="slide flex items-center justify-center h-[100%]">
+                        <div className='forImage flex flex-1 justify-center items-center h-[100%]'>
+                            <img
+                            className="h-[100%] object-cover" 
+                            src={slide.src}
+                            alt="slide+image"/>
+                        </div>
+                        <div className='des flex flex-col flex-1 place-items-start justify-center -ml-11'>
+                            <h2 className="text-[55px] mr-[10px]"> {slide.content.h2} </h2>
+                            <p className="text-[30px] mb-[20px]">{slide.content.p}</p>
+                            <button className='btn'>Shop Now</button>
+                        </div>
+                    </div>
+                </div>
+                )
+            }
+        })}
    
         {/* rightarrow div */}
         <div className={arrowStyle}>
-            <ArrowRightOutlined style={{fontSize : '50px'}}/>
+            <ArrowRightOutlined style={{fontSize : '50px'}} onClick = {nextSlide}/>
         </div>
 
     </div>
