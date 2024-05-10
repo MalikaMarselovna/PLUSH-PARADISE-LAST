7e15e9ab0c5359c03c7d269f83243227b4a75c6b
MalikaMarselovna <148754005+MalikaMarselovna@users.noreply.github.com>
2024-05-10 21:09:19 +0600

    START to make categories

---
+++ src/apiFolder/CategoryApi.js
@@ -0,0 +1,37 @@
+export const ApiCotigories = [
+    {
+        title: '',
+        src: "https://amigurumi.today/wp-content/uploads/2023/06/Free-pink-giraffe-crochet-pattern.jpg"
+    },
+    {
+        title: 'ANIMALS',
+        src: "https://i.pinimg.com/564x/d2/f3/ae/d2f3ae6b61fa372d0752cac645ab5c4f.jpg"
+      
+    },
+    {
+        title: 'DOLLS',
+        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg"
+    },
+    {
+        title: 'RATTLES',
+        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg"
+    },
+    {
+        title: 'CARTOON CHARACTERS',
+        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg"
+    },
+    {
+        title: 'PAIRS',
+        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg"
+    },
+    {
+        title: 'GEOMETRIC FIGURES',
+        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg"
+    },
+    {
+        title: 'KEY RINGS',
+        src: "https://i.pinimg.com/736x/91/1e/55/911e5546cee0bd17cfbf2d7d5f8ce498.jpg"
+    },
+    
+    
+];
---
+++ src/components/Categories.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Categories() {
+  return (
+    <div className='flex justify-between items-center p-5
+'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+    </div>
+  )
+}
+
+export default Categories
